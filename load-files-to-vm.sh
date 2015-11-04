64df44a1b5ad947d607c534c1342991f856ed80f
robertsanders <rssanders3@gmail.com>
2015-11-04 15:08:04 -0700

    Fixing load-files-to-vm.sh script. Updating the Setup step with the new VM instructions.

--- load-files-to-vm.sh
+++ load-files-to-vm.sh
@@ -1,5 +1,8 @@
 # Upload needed files to VM
 
-scp -r -P 2222 spark_workshop_codebase cloudera@localhost:/home/cloudera/spark-workshop/
-scp -r -P 2222 spark_workshop_data cloudera@localhost:/home/cloudera/spark-workshop/
+ssh -n -p 2222 cloudera@localhost "
+	mkdir -p /home/cloudera/spark-workshop/
+"
+scp -r -P 2222 spark_workshop_codebase cloudera@localhost:/home/cloudera/spark-workshop
+scp -r -P 2222 spark-workshop-data cloudera@localhost:/home/cloudera/spark-workshop
 scp -P 2222 load-data-into-hdfs.sh cloudera@localhost:/home/cloudera/spark-workshop/
