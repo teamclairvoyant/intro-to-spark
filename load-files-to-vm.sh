5247bb5d9368d0be868a10655b6c7cd6d7cfebc3
robertsanders <rssanders3@gmail.com>
2015-11-06 14:01:06 -0700

    Adding final slides and updating schedule. Also modifying the load-files-to-vm.sh file.

--- load-files-to-vm.sh
+++ load-files-to-vm.sh
@@ -6,3 +6,8 @@ ssh -n -p 2222 cloudera@localhost "
 scp -r -P 2222 spark_workshop_codebase cloudera@localhost:/home/cloudera/spark-workshop
 scp -r -P 2222 spark-workshop-data cloudera@localhost:/home/cloudera/spark-workshop
 scp -P 2222 load-data-into-hdfs.sh cloudera@localhost:/home/cloudera/spark-workshop/
+ssh -n -p 2222 cloudera@localhost "
+	cd /home/cloudera/spark-workshop
+	find . -name \"*.iml\" -type f -delete
+    find . -name \".DS_Store\" -type f -delete
+"
